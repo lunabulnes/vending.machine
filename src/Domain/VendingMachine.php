05f14bead6067fac6e7925533739c555eaba2686
luna.bulnes <luna.bulnes@gmail.com>
2021-08-26 18:34:30 +0200

    Renamed Change to Cash

--- src/Domain/VendingMachine.php
+++ src/Domain/VendingMachine.php
@@ -3,36 +3,36 @@ declare(strict_types = 1);
 
 namespace App\Domain;
 
-use App\Domain\Money\Change;
+use App\Domain\Money\Cash;
 use App\Domain\Money\Coin;
 
 class VendingMachine
 {
-    private $userChange;
+    private $userCash;
 
-    private function __construct(Change $userChange){
-        $this->userChange = $userChange;
+    private function __construct(Cash $userCash){
+        $this->userCash = $userCash;
     }
 
     public static function create()
     {
-        return new self(Change::create());
+        return new self(Cash::create());
     }
 
     public function addUserCoin(Coin $coin): void
     {
-        $this->userChange->addCoin($coin);
+        $this->userCash->addCoin($coin);
     }
 
     public function getUserMoney(): float
     {
-        return $this->userChange->getTotalMoney();
+        return $this->userCash->getTotalMoney();
     }
 
     public function returnUserMoney(): array
     {
-        $userCoins = $this->userChange->returnMoney();
-        $this->userChange->resetMoney();
+        $userCoins = $this->userCash->returnMoney();
+        $this->userCash->resetMoney();
         return $userCoins;
     }
 }
