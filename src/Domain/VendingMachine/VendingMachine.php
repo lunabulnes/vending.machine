4f59bb63ff803e7253b67b35721d2e533b793173
luna.bulnes <luna.bulnes@gmail.com>
2021-08-28 23:44:14 +0200

    Added extra new lines

--- src/Domain/VendingMachine/VendingMachine.php
+++ src/Domain/VendingMachine/VendingMachine.php
@@ -13,13 +13,22 @@ use JsonSerializable;
 interface VendingMachine extends JsonSerializable
 {
     public function getCatalog(): Catalog;
+
     public function addUserCoin(Coin $coin): void;
+
     public function getUserMoney(): int;
+
     public function returnUserCoins(): array;
+
     public function getMachineMoney(): int;
+
     public function buy(Product $product): Purchase;
+
     public function addStock(Stock $catalog): void;
+
     public function refillChange(Money $change): void;
+
     public function startMaintenance(): void;
+
     public function stopMaintenance(): void;
 }
